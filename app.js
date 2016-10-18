c77c2978c764fdaa37b988aeec15888d4403a6e5
Laure <laure.milian@gmail.com>
2016-10-18 12:22:35 +0200

    exercice this

--- app.js
+++ app.js
@@ -1,57 +1,81 @@
+"use strict";
+
 (function(){
+
 	var app = {
 
 		time : null,
 		intervalID: null,
 		default: 6,
 		timeMax: 6,
-		
+
 		init : function() {
-			app.listeners();
-			app.time = app.default;
-			app.timeSeparation();
+			this.listeners();
+			this.time = this.default;
+			this.timeSeparation();
 		},
 
 		listeners : function() {
-			$("#btnstart").on("click", app.start);
-			$("#btnstop").on("click", app.stop);
-			$("#btnpause").on("click", app.pause);
-			$("#btnreset").on("click", app.reset);
-			$("#inputUser").on("input", app.inputUser);
+			$("#btnstart").on("click", this.start.bind(this));
+			$("#btnstop").on("click", this.stop.bind(this));
+			$("#btnpause").on("click", this.pause.bind(this));
+			$("#btnreset").on("click", this.reset.bind(this));
+			$("#inputUserMinutes, #inputUserSeconds").on("input", this.inputUser.bind(this));
 		},
 
 		inputUser : function() {
-			app.time = app.timeMax = $("#inputUser").val();
-			if (app.time >= 0) {
-				app.timeSeparation();
+			var inputUserMinutes = parseInt($("#inputUserMinutes").val(), 10);
+			var inputUserSeconds = parseInt($("#inputUserSeconds").val(), 10);
+			if (inputUserSeconds && inputUserMinutes) {
+				this.time = this.timeMax = inputUserMinutes * 60 + inputUserSeconds ;
+			} else if (inputUserMinutes) {
+				this.time = this.timeMax = inputUserMinutes * 60 ;
+			} else if (inputUserSeconds) {
+				this.time = this.timeMax = inputUserSeconds;
+			}
+			this.timeSeparation();
+		},
+
+		start : function() {	
+			if (this.intervalID === null) {
+				this.intervalID = setInterval(function() {
+					if (this.time > 0) {
+						this.time--;
+						this.timeSeparation();
+					} else {
+						this.videoLaunch();
+					}
+				}.bind(this), 1000);
 			}
 		},
 
+		/* OU on peut utiliser
 		start : function() {
-			if (app.intervalID === null) {
-				app.intervalID = setInterval(function() {
-					if (app.time > 0) {
-						app.time--;
-						app.timeSeparation();
+			var self = this;	
+			if (this.intervalID === null) {
+				this.intervalID = setInterval(function() {
+					if (self.time > 0) {
+						self.time--;
+						self.timeSeparation();
 					} else {
-						app.videoLaunch();
+						self.videoLaunch();
 					}
 				}, 1000);
 			}
-		},
+		},*/
 
 
 		stop : function() {
-			clearInterval(app.intervalID);
-			app.intervalID = null;
+			clearInterval(this.intervalID);
+			this.intervalID = null;
 		},
 
 		timeSeparation : function() {
-			var minutes = Math.floor(app.time / 60);
-			var seconds = Math.floor(app.time % 60);
-			app.timeDisplay(minutes, "#minutes");
-			app.timeDisplay(seconds, "#seconds");
-			app.displayProgressBar();
+			var minutes = Math.floor(this.time / 60);
+			var seconds = Math.floor(this.time % 60);
+			this.timeDisplay(minutes, "#minutes");
+			this.timeDisplay(seconds, "#seconds");
+			this.displayProgressBar();
 		},
 
 		timeDisplay : function(timeElement, selector) {
@@ -63,29 +87,35 @@
 		},
 
 		displayProgressBar : function() {
-			var progressPercent = Math.floor(100 - ((app.time / app.timeMax) * 100));
+			var progressPercent = Math.floor(100 - ((this.time / this.timeMax) * 100));
 			$("#progress").css("width", progressPercent + "%");
 		},
 
 		pause : function() {
-			if (app.intervalID !== null) {
-				app.stop();
+			if (this.intervalID !== null) {
+				this.stop();
 				$("#btnpause").html("Play ");		
 			} else {
-				app.start();
+				this.start();
 				$("#btnpause").html("Pause");
 			}
 		},
 
 		reset : function() {
-			app.time = $("#inputUser").val();
-			app.timeSeparation();
-			app.stop();
+			this.inputUser();
+			this.timeSeparation();
+			this.stop();
 		},
 
 		videoLaunch : function() {
-			app.stop();
+			this.stop();
 			$("#video").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/7jWYUtQZhK0?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+			$("#inputUserMinutes").on("input", this.videoStop);
+			$("#inputUserSeconds").on("input", this.videoStop);
+		},
+
+		videoStop : function() {
+			$("#video").html("");
 		}
 
 	}
